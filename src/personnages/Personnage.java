59628acc6d07f3dd6522baa899d18e9d4a482ac1
ml-Dev-K <kanemouhamadoulamine50@gmail.com>
2025-03-20 16:44:11 +0100

    first Commit

---
+++ src/personnages/Personnage.java
@@ -0,0 +1,10 @@
+package personnages;
+
+public class Personnage {
+    private String nom;
+    private String nomImage;
+    private String[] dialogue;
+    private String description;
+    private Object[] listeObjet;
+
+}
